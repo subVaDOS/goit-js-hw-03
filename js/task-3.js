ca3d35a3d1ada9e43a466cc1bdcc51581c1525b8
Manokha Vadim <manokha.vadim@gmail.com>
2025-03-21 19:58:15 +0200

    modified js3

--- js/task-3.js
+++ js/task-3.js
@@ -16,23 +16,3 @@ console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
-
-// const planets = ['Earth', 'Mars', 'Venus'];
-
-// for (let i = 0; i < planets.length; i += 1) {
-//   console.log(planets[i]);
-// }
-// const planets = ["Earth", "Mars", "Venus"];
-
-// console.log(planets.includes("Earth")); // true
-// console.log(planets.includes("Mars")); // true
-// console.log(planets.includes("Venus")); // true
-// console.log(planets.includes("Jupiter")); // false
-
-// const fruits = ['apple', 'banana', 'orange'];
-
-// if (fruits.includes('banana')) {
-//   console.log('The array has an element banana');
-// } else {
-//   console.log('Array does not contain banana element');
-// }
