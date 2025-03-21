592c5cffc66694cc53a11e2fdec818df8a078201
Manokha Vadim <manokha.vadim@gmail.com>
2025-03-21 13:01:12 +0200

    modified files js

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ function slugify(title) {
   return title.toLowerCase().split(' ').join('-');
 }
 
-// Перевірка
+// Audit
 console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
 console.log(slugify('English for developer')); // "english-for-developer"
 console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
--- js/task-2.js
+++ js/task-2.js
@@ -3,7 +3,7 @@ function makeArray(firstArray, secondArray, maxLength) {
   return newArray.slice(0, maxLength);
 }
 
-// Перевірка
+// Audit
 console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
 console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
