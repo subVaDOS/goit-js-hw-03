fdf54e0f118bc863626d6aa2f5c9bc8a054295ec
Manokha Vadim <manokha.vadim@gmail.com>
2025-03-21 19:49:01 +0200

    modified file

--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,38 @@
-// function filterArray(numbers, value) {
-//   const filteredNumbers = [];
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
 
-//   for (const number of numbers) {
-//     if (number > value) {
-//       filteredNumbers.push(number);
-//     }
-//   }
+  for (const number of numbers) {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
 
-//   return filteredNumbers;
-// }
-
-// // Провіремо роботу функції
-// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
-
-const planets = ['Earth', 'Mars', 'Venus'];
-
-for (let i = 0; i < planets.length; i += 1) {
-  console.log(planets[i]);
+  return filteredNumbers;
 }
+
+// Провіремо роботу функції
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// const planets = ['Earth', 'Mars', 'Venus'];
+
+// for (let i = 0; i < planets.length; i += 1) {
+//   console.log(planets[i]);
+// }
+// const planets = ["Earth", "Mars", "Venus"];
+
+// console.log(planets.includes("Earth")); // true
+// console.log(planets.includes("Mars")); // true
+// console.log(planets.includes("Venus")); // true
+// console.log(planets.includes("Jupiter")); // false
+
+// const fruits = ['apple', 'banana', 'orange'];
+
+// if (fruits.includes('banana')) {
+//   console.log('The array has an element banana');
+// } else {
+//   console.log('Array does not contain banana element');
+// }
