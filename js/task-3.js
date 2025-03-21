26afbea2d45e16469a2e23620949abf51a8ad902
Manokha Vadim <manokha.vadim@gmail.com>
2025-03-21 13:49:32 +0200

    modified file html js

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,24 @@
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
 
-  for (const number of numbers) {
-    if (number > value) {
-      filteredNumbers.push(number);
-    }
-  }
+//   for (const number of numbers) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
 
-  return filteredNumbers;
+//   return filteredNumbers;
+// }
+
+// // Провіремо роботу функції
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+const planets = ['Earth', 'Mars', 'Venus'];
+
+for (let i = 0; i < planets.length; i += 1) {
+  console.log(planets[i]);
 }
-
-// Провіремо роботу функції
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
